72247c08e5bc77f36fe07a2a23bfbc6fa67e9c8d
hhkarsten <hhkarsten@googlemail.com>
2018-09-04 15:11:54 +0200

    first useful changes

--- agile_calendar.rb
+++ agile_calendar.rb
@@ -1,5 +1,13 @@
 #!/usr/bin/env ruby
 
+$LOAD_PATH << '.'
+require "calendar_helper"
+
+class MyCal
+	include CalendarHelper
+end
+
+
 def put_line
 	puts "---------------------------------------------------------------------------------\n\n"
 end
@@ -10,17 +18,20 @@ index = 0
 done = Array.new
 done[index] = r
 
-put_line
+#put_line
 
 25.times do
 	while ! done.include?(r) do
 		r = rand(lines.size)
 	end
 	r = rand(lines.size)
-	puts lines[r]
+#	puts lines[r]
 	index += 1
 	done[index] = r
 end
 
-put_line
+#put_line
 
+
+c = MyCal.new.calendar(:year => 2018, :month => 9, :table_class => "calendar_helper", :abbrev => true, :first_day_of_week => 1)
+puts c
