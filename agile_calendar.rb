203446e49ac450cdf875022943142a125531191c
hhkarsten <hhkarsten@googlemail.com>
2018-09-04 16:08:16 +0200

    added block for agile topic of the day and some css tweaks

--- agile_calendar.rb
+++ agile_calendar.rb
@@ -14,7 +14,7 @@ index = 0
 done = Array.new
 done[index] = r
 
-25.times do
+31.times do
 	while ! done.include?(r) do
 		r = rand(lines.size)
 	end
@@ -23,6 +23,30 @@ done[index] = r
 	index += 1
 	done[index] = r
 end
+listOfSpecialDays = Array.new()
 
-c = MyCal.new.calendar(:year => 2018, :month => 9, :table_class => "calendar_helper", :abbrev => true, :first_day_of_week => 1, :show_today => false)
+c = MyCal.new.calendar(:year => 2018,
+                       :month => 10,
+                       :table_class => "calendar_helper",
+                       :abbrev => 0..-1,
+                       :first_day_of_week => 1,
+                       :show_today => false,
+                       :calendar_title => "Agile calendar")  do |d|
+	if ! d.saturday? && ! d.sunday? then
+		content = d.mday.to_s + "</br>" + lines[d.mday].to_s
+		[content]
+	end
+end
+
+
+
+#   calendar(:year => 2005, :month => 5) do |d| # This generates a simple calendar, but gives special days
+#     if listOfSpecialDays.include?(d)          # (days that are in the array listOfSpecialDays) one CSS class,
+#       [d.mday, {:class => "specialDay"}]      # "specialDay", and gives the rest of the days another CSS class,
+#     else                                      # "normalDay". You can also use this highlight today differently
+#       [d.mday, {:class => "normalDay"}]       # from the rest of the days, etc.
+#     end
+#   end
+
 puts c
+
